4faf1d78250a2c0ab383a56d18957cd3413f3e9a
Sakib Khan <sakibkhan424@gmail.com>
2020-08-20 18:28:38 +0530

    responsive

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from './elements/Header.js'
 import Home from './Home.jsx'
 
 const App = () =>(
-<div className='bg-background pr-4'>
+<div className='bg-background '>
  <Header />
   <Home />
   </div>
--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => (
-<div className='mx-24 py-10 flex justify-between items-center px-6 '> 
+<div className=' pt-4 md:mx-24 md:py-10 flex justify-between items-center md:px-6  flex-col md:flex-row'> 
     <div className="flex justify-evenly items-center">
     <svg viewBox="0 0 20 20" fill="currentColor" className="play w-16 h-16 text-xl "><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" /></svg>
     <h1 className='font-sans text-3xl font-bold tracking-wide'>Movie-Box</h1>
