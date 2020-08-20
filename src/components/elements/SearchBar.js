eff8413faad66bab7929aacfb6b0a155e4da52c6
Sakib Khan <sakibkhan424@gmail.com>
2020-08-20 23:22:09 +0530

    :incomplet222332e

--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -3,8 +3,8 @@ import './SearchBar.css'
 
 const SearchBar = () =>(
     <>
-     <div className='md:ml-32 md:mr-32 flex justify-center flex-col items-center md:items-stretch pb-6 pt-4 md:pt-0 '>
-         <h1 className='font-sans text-xl md:text-left text-center md:text-3xl font-bold tracking-wide pb-12'>Search For Your Favourite Movies Here!!</h1>
+     <div className='md:ml-32 md:mr-32 flex justify-center flex-col items-center md:items-stretch pb-6 pt-4 md:pt '>
+         <h1 className='font-sans text-xl md:text-left text-center md:text-3xl font-bold tracking-wide pb-4 md:pb-12'>Search For Your Favourite Movies Here!!</h1>
          <form>
          <input class=" css bg-background focus:outline-none   border-b-1 border-gray-900 rounded-sm py-2 px-4 block w-full appearance-none leading-normal placeholder-gray-600 focus:placeholder-gray-900 transform focus:-translate-y-1 focus:scale-y-150 transition duration-500 ease-in-out" type="email" placeholder="Search Here" />
 </form>
