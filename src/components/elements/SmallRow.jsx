b5569768b91a50117bcb2e54bb3ed57ba11bc2f8
Sakib Khan <sakibkhan424@gmail.com>
2020-10-18 01:19:49 +0530

    link

--- src/components/elements/SmallRow.jsx
+++ src/components/elements/SmallRow.jsx
@@ -6,7 +6,7 @@ const SmallRow = () => (
   <div className="md:text-3xl font-bold font-gray-900 pt-6 pb-84
   text-lg text-center md:text-left">
    <h1 className='pb-24 pt-12'>Sorry!! No movies Found :( </h1>
-    <LoadMoreBtn text="Go Back!" link='https://movie-box-three.vercel.app/' />
+    <LoadMoreBtn text="Go Back!" link='https://movie-boxv1.vercel.app/' />
   </div>
 );
  
